7799c657ea185b4467b1792905f179dd096e02c0
Mason Malone <651224+MasonM@users.noreply.github.com>
2026-06-06 11:38:39 -0700

    Nginx tweaks
    Signed-off-by: Mason Malone <651224+MasonM@users.noreply.github.com>

--- shellbox/update.sh
+++ shellbox/update.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 # ssh shellbox.dev create-from-oci masonm docker.io/library/php:8.5.6-fpm-trixie
 
-ssh masonm@sb 'apt-get install -y php8.4-fpm nginx git rsync vim'
+ssh masonm@sb 'command -v nginx || apt-get install -y php8.4-fpm nginx git rsync vim'
 rsync -avz nginx masonm@sb:/etc/
 rsync -avz --usermap='*:www-data' --groupmap='*:www-data' www/masonm.org masonm@sb:/var/www/
 ssh masonm@sb 'service nginx restart'
